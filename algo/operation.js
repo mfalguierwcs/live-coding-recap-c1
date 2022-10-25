33f0af9791730b6bc362a9287ca1d2451431bed8
mfalguierwcs <maxime.falguier@wildcodeschool.com>
2022-10-25 15:53:25 +0200

    algo 2 done

--- algo/operation.js
+++ algo/operation.js
@@ -2,8 +2,13 @@
 Elle doit retourner le résultat de la soustraction si il est 
 supérieur à 0 et si c'est bien un nombre sinon false */
 
-const soustraction = () => {
-    
+const soustraction = (a, b) => {
+    const result = a - b;
+    if (result > 0) {
+        return result
+    } else {
+        return false
+    }
 }
 
 module.exports = soustraction
