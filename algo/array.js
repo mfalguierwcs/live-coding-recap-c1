c0fdb7ee07e6673a33302e803be070c625b3ab5c
mfalguierwcs <maxime.falguier@wildcodeschool.com>
2022-11-05 23:02:48 +0100

    live co done

--- algo/array.js
+++ algo/array.js
@@ -1,8 +1,10 @@
 // Retourner l'url d'un film en cherchant à partir de son nom exact (title),  
 // s'aider de array.test.js
 
-const findMovieUrlByTitle = () => {
-    
+const findMovieUrlByTitle = (data, title) => {
+    console.log(title)
+    const movie = data.find((movie) => movie.title === title)
+    return movie.poster_path;
 }
 
 module.exports = findMovieUrlByTitle
--- script/main.js
+++ script/main.js
@@ -5,6 +5,7 @@ const wilders = [
     "Margaux",
     "Marion",
     "Danyel",
+    "Alban",
     "Bertrand",
     "Samy",
     "Elodie",
@@ -12,47 +13,82 @@ const wilders = [
     "David",
     "JB",
     "Denis",
-    "Vincent"
+    "Vincent",
+    "Nadège",
+    "Nicolas",
+    "Julien"
 ]
 
 // j'écris mes sélecteurs
+const help = document.getElementById("help");
+const helpPerson = document.getElementById("personHelp");
 
 // je transforme ma fonction avec la synthaxe es6
 function helpLiveCoding() {
-    // Math.round(Math.random() * wilders.length)
+    const randomWilder = Math.round(Math.random() * (wilders.length - 1));
+    const wilder = wilders[randomWilder];
+    helpPerson.innerText = wilder;
 }
 
 // comment écrit-on un listener qui appelle ma fonction ?
+help.addEventListener("click", () => {
+    helpLiveCoding();
+})
 
 
 
 // js dom 2 - génerateur de bons films star wars
-const btnGenerator = document.querySelector("#generator")
 const imgArea = document.querySelector("#imageArea")
 
 const dataSw = [{
-    poster_path: "/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg",
+    poster_path: "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/6FfCtAuVAW8XJjZ7eWeLibRLWTw.jpg",
     vote_average: 8.2,
     title: "Star Wars"
 }, {
-    poster_path: "/tEQlCGiiWvMvfD7Sz8d99Pouy39.jpg",
+    poster_path: "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/tEQlCGiiWvMvfD7Sz8d99Pouy39.jpg",
     vote_average: 8.5,
     title: "Star Wars: The return of jedi"
 }, {
     title: "Star Wars: The Last Jedi",
     vote_average: 6.9,
-    poster_path: "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+    poster_path: "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
 },
 {
     title: "Star Wars: The Force Awakens",
     vote_average: 7.3,
-    poster_path: "/wqnLdwVXoBjKibFRR5U3y0aDUhs.jpg",
+    poster_path: "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/wqnLdwVXoBjKibFRR5U3y0aDUhs.jpg",
 },
 {
     title: "Rogue One: A Star Wars Story",
     vote_average: 7.5,
-    poster_path: "/i0yw1mFbB7sNGHCs7EXZPzFkdA1.jpg",
+    poster_path: "https://www.themoviedb.org/t/p/w188_and_h282_bestv2/i0yw1mFbB7sNGHCs7EXZPzFkdA1.jpg",
 }]
+
+const generatePictures = () => {
+    let cpt = 0;
+    const img = document.createElement("img");
+    img.src = dataSw[cpt].poster_path;
+    imgArea.appendChild(img);
+    
+    setInterval(() => {
+        if (cpt < dataSw.length - 1) {
+            cpt++
+        } else {
+            cpt = 0
+        }
+
+        const film = dataSw[cpt];
+        img.src = film.poster_path;
+            
+    }, 3000)
+    
+}
+
+generatePictures();
+
+
+
+
 
 
 
@@ -60,5 +96,9 @@ const dataSw = [{
 // js dom 3 - multiple selector
 const swTtl = document.querySelectorAll(".sw-ttl");
 
-
+swTtl.forEach((elementTtl) => {
+    elementTtl.addEventListener("mouseover", () => {
+        elementTtl.style.color = "red";
+    })
+})
 
